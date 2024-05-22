f26ae2576f905d6d23f9acc57117bf8cb6e3cd18
Jacob Cooper <JacobCooper2302@gmail.com>
2024-05-22 13:25:07 +1000

    first commit

---
+++ src/util/config.js
@@ -0,0 +1,6 @@
+
+export let api = 'http://192.168.1.22';
+export const config = {
+  //BASE_URL: `${api}:5000/api`, 
+  BASE_URL: 'https://beanscenefoodapp-jacob-e18110fc7f5b.herokuapp.com/api'
+};
